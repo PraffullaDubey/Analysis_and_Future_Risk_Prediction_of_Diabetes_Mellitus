f17ac46c639f3b0f7eac2a4b98d44f0c5a2bf431
Praffulla Dubey <praffullakrdubey@gmail.com>
2021-02-16 12:14:06 +0530

    Added Cleaning

---
+++ Major Project/New folder/ml_app_dia.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+import pickle
+
+pickle_in = open('logisticRegr.pkl', 'rb')
+classifier = pickle.load(pickle_in)
+
+st.sidebar.header('Diabetes Prediction')
+select = st.sidebar.selectbox('Select Form', ['Form 1'], key='2')
+if not st.sidebar.checkbox("Hide", True, key='2'):
+    st.title('Diabetes Prediction(Only for females above 21years of Age)')
+    name = st.text_input("Name:")
+    pregnancy = st.number_input("No. of times pregnant:")
+    glucose = st.number_input("Plasma Glucose Concentration :")
+    bp =  st.number_input("Diastolic blood pressure (mm Hg):")
+    skin = st.number_input("Triceps skin fold thickness (mm):")
+    insulin = st.number_input("2-Hour serum insulin (mu U/ml):")
+    bmi = st.number_input("Body mass index (weight in kg/(height in m)^2):")
+    dpf = st.number_input("Diabetes Pedigree Function:")
+    age = st.number_input("Age:")
+
+    submit = st.button('Predict')
+
+    if submit:
+        prediction = classifier.predict([[pregnancy, glucose, bp, skin, insulin, bmi, dpf, age]])
+        if prediction == 0:
+            st.write('Congratulation',name,'You are not diabetic')
+        else:
+            st.write(name," we are really sorry to say but it seems like you are Diabetic. But don't lose hope we have suggestions for you:")
